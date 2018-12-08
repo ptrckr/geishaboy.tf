b0b4a62da2328388ecd8312186ce02a998ea4617
ptrckr <github@ptrckr.tokyo>
2018-12-08 01:18:33 +0100

    Rebuild

--- scripts/background.js
+++ scripts/background.js
@@ -28,7 +28,6 @@ const stamp = new Image();
 stamp.onload = () => {
   for (let collection = 0; collection < Math.min(collections.length, angles_count); ++collection) {
     const current_collection = collections[collection].effects.filter(x => x.collected).map(x => x.id);
-    console.log(current_collection);
     let angle = angles_to_paint.shift();
 
     for (let i = 0; i < current_collection.length; i++) {
@@ -56,6 +55,35 @@ stamp.onload = () => {
         ctx.translate(-x, -y);
 
         ctx.drawImage(geisha, x - stamp_size / 2 + 10, y - stamp_size / 2 + 10, stamp_size - 20, stamp_size - 20);
+
+        ctx.shadowColor = "black";
+        ctx.shadowBlur = 40;
+        ctx.drawImage(stamp, x - stamp_size / 2, y - stamp_size / 2, stamp_size, stamp_size);
+
+        ctx.restore();
+      }
+
+      geisha.onerror = () => {
+        ctx.save();
+
+        let rotation = 90 + angle;
+        let radius = main_p_width - 200;
+
+        if (i > 0) {
+          angle += Math.floor(Math.random() * 3) * (Math.random() > 0.5 ? 1 : -1);
+          rotation += Math.floor(Math.random() * 30) * (Math.random() > 0.5 ? 1 : -1);
+          radius += Math.floor(Math.random() * 300);
+        }
+
+        let x = (width / 2) + radius * Math.cos(angle * Math.PI / 180);
+        let y = (height / 2) + radius * Math.sin(angle * Math.PI / 180);
+
+        ctx.translate(x, y);
+        ctx.rotate(rotation * Math.PI / 180);
+        ctx.translate(-x, -y);
+
+        ctx.fillStyle = "purple";
+        ctx.fillRect(x - stamp_size / 2 + 10, y - stamp_size / 2 + 10, stamp_size - 20, stamp_size - 20);
 
         ctx.shadowColor = "black";
         ctx.shadowBlur = 40;
