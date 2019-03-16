96b235855e23b9a67fc3ab2519a664eeb22569db
ptrckr <github@ptrckr.tokyo>
2019-03-16 21:33:32 +0100

    New design, replaced jekyll with pug

---
+++ collection.js
@@ -0,0 +1,395 @@
+const collection = {};
+
+collection.series = [
+  { name: "Gen I",
+    effects: [
+      {
+        collected: true,
+        name: "Burning Flames",
+        id: 13,
+        item_id: 962695959
+      }, {
+        collected: true,
+        name: "Scorching Flames",
+        id: 14,
+        item_id: 320595946
+      }, {
+        id: 7,
+        name: "Purple Confetti",
+        collected: true,
+        item_id: 364908286,
+        duped: true
+      }, {
+        id: 6,
+        name: "Green Confetti",
+        collected: true,
+        item_id: 698267181
+      }, {
+        id: 10,
+        name: "Purple Energy",
+        collected: true,
+        item_id: 5909365703
+      }, {
+        id: 9,
+        name: "Green Energy",
+        collected: true,
+        item_id: 281221108,
+        duped: true
+      }, {
+        id: 15,
+        name: "Searing Plasma",
+        collected: true,
+        item_id: 334874778
+      }, {
+        id: 16,
+        name: "Vivid Plasma",
+        collected: true,
+        item_id: 461197764
+      }, {
+        id: 18,
+        name: "Circling Peace Sign",
+        collected: true,
+        item_id: 3098524582
+      }, {
+        id: 11,
+        name: "Circling TF Logo",
+        collected: true,
+        item_id: 309667579,
+        duped: true
+      }, {
+        id: 19,
+        name: "Circling Heart",
+        collected: true,
+        item_id: 416986640
+      }, {
+        id: 17,
+        name: "Sunbeams",
+        collected: true,
+        item_id: 434284419
+      }, {
+        id: 8,
+        name: "Haunted Ghosts",
+        collected: true,
+        item_id: 2503844159
+      }, {
+        id: 12,
+        name: "Massed Flies",
+        collected: true,
+        item_id: 383603588,
+        duped: true
+      }
+  ]},  // Gen I
+  { name: "Gen II",
+    effects: [
+      {
+        id: 30,
+        name: "Blizzardy Storm",
+        collected: true,
+        item_id: 505671006,
+        duped: true
+      }, {
+        id: 29,
+        name: "Stormy Storm",
+        collected: true,
+        item_id: 1121266129
+      }, {
+        id: 36,
+        name: "Steaming",
+        collected: true,
+        item_id: 1495545775
+      }, {
+        id: 35,
+        name: "Smoking",
+        collected: true,
+        item_id: 584233007
+      }, {
+        id: 33,
+        name: "Orbiting Fire",
+        collected: true,
+        item_id: 475647825
+      }, {
+        id: 32,
+        name: "Orbiting Planets",
+        collected: true,
+        item_id: 899870709
+      }, {
+        id: 34,
+        name: "Bubbling",
+        collected: true,
+        item_id: 1334635171
+      }, {
+        id: 31,
+        name: "Nuts n' Bolts",
+        collected: true,
+        item_id: 1001721440
+      }
+  ]},  // Gen II
+  { name: "Gen III",
+    effects: [
+      {
+        id: 56,
+        name: "Kill-a-Watt",
+        collected: true,
+        item_id: 2206659370
+      }, {
+        id: 57,
+        name: "Terror-Watt",
+        collected: true,
+        item_id: 1779250244
+      }, {
+        id: 61,
+        name: "Miami Nights",
+        collected: true,
+        item_id: 2842719449
+      }, {
+        id: 62,
+        name: "Disco Beat Down",
+        collected: true,
+        item_id: 2811000132
+      }, {
+        id: 58,
+        name: "Cloud 9",
+        price: 50
+      }, {
+        id: 59,
+        name: "Aces High",
+        collected: true,
+        item_id: 1927487388
+      }, {
+        id: 60,
+        name: "Dead Presidents",
+        collected: true,
+        item_id: 2636822018
+      }
+  ]},  // Gen III
+  { name: "Very Scary Halloween Special",
+    effects: [
+      {
+        id: 38,
+        name: "Cloudy Moon",
+        collected: true,
+        item_id: 1248613524
+      }, {
+        id: 37,
+        name: "Flaming Lantern",
+        collected: true,
+        item_id: 1250245583
+      }, {
+        id: 40,
+        name: "Eerie Orbiting Fire",
+        collected: true,
+        item_id: 566860751
+      }, {
+        id: 39,
+        name: "Cauldron Bubbles",
+        price: 100
+      }
+  ]},  // Very Scary Halloween Special
+  { name: "Spectral Halloween Special",
+    effects: [
+      {
+        id: 45,
+        name: "Harvest Moon",
+        collected: true,
+        item_id: 4311410370
+      }, {
+        id: 46,
+        name: "It's A Secret To Everybody",
+        collected: true,
+        item_id: 1198650747
+      }, {
+        id: 43,
+        name: "Knifestorm",
+        collected: true,
+        item_id: 1196236313
+      }, {
+        id: 47,
+        name: "Stormy 13th Hour",
+        price: 250
+      }, {
+        id: 44,
+        name: "Misty Skull",
+        price: 110
+      }
+  ]},  // Spectral Halloween Special
+  { name: "Scream Fortress 2013",
+    effects: [
+      {
+        id: 73,
+        name: "Arcana",
+        collected: true,
+        item_id: 2066097384
+      }, {
+        id: 74,
+        name: "Spellbound",
+        collected: true,
+        item_id: 2040823388
+      }, {
+        id: 75,
+        name: "Chiroptera Venenata",
+        collected: true,
+        item_id: 2081077225
+      }, {
+        id: 76,
+        name: "Poisoned Shadows",
+        price: 150,
+        unknown: true
+      }, {
+        id: 77,
+        name: "Something Burning This Way Comes",
+        price: 150,
+        unknown: true
+      }, {
+        id: 78,
+        name: "Hellfire",
+        collected: true,
+        item_id: 2080499934
+      }, {
+        id: 79,
+        name: "Darkblaze",
+        collected: true,
+        item_id: 2040620622
+      }, {
+        id: 80,
+        name: "Demonflame",
+        price: 200,
+        unknown: true
+      }
+  ]},  // Scream Fortress 2013
+  { name: "Scream Fortress 2014",
+    effects: [
+      {
+        id: 82,
+        name: "Amaranthine",
+        collected: true,
+        item_id: 3184787941
+      }, {
+        id: 83,
+        name: "Stare From Beyond",
+        collected: true,
+        item_id: 3129024647
+      }, {
+        id: 84,
+        name: "The Ooze",
+        collected: true,
+        item_id: 3167827397
+      }, {
+        id: 85,
+        name: "Ghastly Ghosts Jr",
+        collected: true,
+        item_id: 3134096578
+      }, {
+        id: 86,
+        name: "Haunted Phantasm Jr",
+        price: 100,
+        unknown: true
+      }, {
+        id: 81,
+        name: "Bonzo The All-Gnawing",
+        collected: true,
+        item_id: 3178322732
+      }
+  ]},  // Scream Fortress 2014
+  { name: "Scream Fortress 2016",
+    effects: [
+      {
+        id: 109,
+        name: "Starstorm Insomnia",
+        price: 90,
+        unknown: true
+      }, {
+        id: 110,
+        name: "Starstorm Slumber",
+        price: 90,
+        unknown: true
+      }, {
+        id: 108,
+        name: "Tesla Coil",
+        price: 40,
+        unknown: true
+      }, {
+        id: 107,
+        name: "Neutron Star",
+        collected: true,
+        item_id: 5187620988
+      }
+  ]},  // Scream Fotress 2016
+  { name: "Robotic Boogaloo",
+    effects: [
+      {
+        id: 70,
+        name: "Time Warp",
+        collected: true,
+        item_id: 1660130830
+      }, {
+        id: 71,
+        name: "Green Black Hole",
+        price: 100
+      }, {
+        id: 63,
+        name: "Phosphorous",
+        collected: true,
+        item_id: 1847675847
+      }, {
+        id: 64,
+        name: "Sulphurous",
+        collected: true,
+        item_id: 3605081878
+      }, {
+        id: 65,
+        name: "Memory Leak",
+        collected: true,
+        item_id: 1661173819
+      }, {
+        id: 66,
+        name: "Overclocked",
+        collected: true,
+        item_id: 1648694518
+      }, {
+        id: 67,
+        name: "Electrostatic",
+        collected: true,
+        item_id: 1657829049
+      }, {
+        id: 68,
+        name: "Power Surge",
+        collected: true,
+        item_id: 1680793205
+      }, {
+        id: 69,
+        name: "Anti-Freeze",
+        collected: true,
+        item_id: 1678449000
+      }, {
+        id: 72,
+        name: "Roboactive",
+        price: 100
+      }
+  ]},  // Robotic Boogaloo
+  { name: "End of the Line",
+    effects: [
+      {
+        id: 89,
+        name: "Morning Glory",
+        collected: true,
+        item_id: 6812241511
+      }, {
+        id: 90,
+        name: "Death at Dusk",
+        collected: true,
+        item_id: 3523882994
+      }, {
+        id: 87,
+        name: "Frostbite",
+        collected: true,
+        item_id: 4330295500
+      }, {
+        id: 88,
+        name: "Molten Mallard",
+        collected: true,
+        item_id: 3270094681
+      }
+  ]}  // End of the Line
+];
+
+module.exports = collection;
---
+++ gruntfile.js
@@ -0,0 +1,74 @@
+const source_dir = "src/";
+const dest_dir = "docs/";
+const config = {
+  // grunt-contrib-copy
+  copy: {
+    main: {
+      files: [{
+        expand: true,
+        cwd: source_dir,
+        src: ["CNAME", "img/**", "favicon.png"],
+        dest: dest_dir
+      }]
+    }
+  },
+
+  // grunt-contrib-cssmin
+  cssmin: {
+    options: {
+      mergeIntoShorthands: true,
+    },
+    target: {
+      files: [{
+        expand: true,
+        cwd: source_dir,
+        src: ["*.css"],
+        dest: dest_dir
+      }]
+    }
+  },
+
+  // grunt-contrib-pug
+  pug: {
+    compile: {
+      options: {
+        data: (src, dest) => {
+          const collection = require("./collection");
+
+          return {
+            url: {
+              backpack: "https://backpack.tf/profiles/76561198060233163",
+              scm: "https://steamcommunity.com/market/listings/440/Unusual%20Geisha%20Boy",
+              profile: "https://steamcommunity.com/id/ptrckr",
+              trade: "https://steamcommunity.com/tradeoffer/new/?partner=99967435&token=eCs5JlvU"
+            },
+            key_price: 1.80,
+            series: collection.series,
+            currently_buying_geishas: false,
+            geishas: {
+              collected: collection.series.reduce((a, s) =>
+                a += s.effects.filter(effect => effect.collected).length, 0),
+              total: collection.series.reduce((a, s) =>
+                a += s.effects.filter(effect => !effect.unknown).length, 0)
+            }
+          };
+        }
+      },
+      files: [{
+        expand: true,
+        cwd: source_dir,
+        src: "index.pug",
+        dest: dest_dir,
+        ext: ".html"
+      }]
+    }
+  }
+};
+
+module.exports = grunt => {
+  grunt.initConfig(config);
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-pug");
+  grunt.registerTask("static_build", ["copy", "cssmin", "pug"]);
+};
